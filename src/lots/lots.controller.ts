6b30a04ab31afdd54d0717dd7d9f270a96e47688
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-10-21 15:31:14 -0400

    Update lots.controller.ts

--- src/lots/lots.controller.ts
+++ src/lots/lots.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('lots')
-export class LotsController {}
+export class LotsController {
+	/*
+		GET /lots
+		GET /lots/:pk_id
+		POST /lots
+		PATCH/PUT /lots/:pk_id
+		DELETE /lots/:pk_id
+	*/
+	@Get() // /lots
+	findAll() {
+		return [];
+	}
+
+	@Get('open') // /lots/open
+	findOpen() {
+		return [];
+	}
+
+	@Get(':pk_id') // /lots/:pk_id
+	findOne(@Param('pk_id') pk_id: string) {
+		return { pk_id };
+	}
+
+	@Post() // /lots
+	create(@Body() lot: {}) {
+		return lot
+	}
+
+	@Patch(':pk_id') // /lots/:pk_id
+	update(@Param('pk_id') pk_id: string, @Body() lotUpdate: {}) {
+		return { pk_id, ...lotUpdate };
+	}
+
+	@Delete(':pk_id') // /lots/:pk_id
+	delete(@Param('pk_id') pk_id: string) {
+		return { pk_id };
+	}
+}
