6256657e0eeb46571462ec22beb7eb64cee7b2f8
JMotDQS <147440538+JMotDQS@users.noreply.github.com>
2024-12-03 12:41:48 -0500

    trying things

--- src/Users/user.model.ts
+++ src/Users/user.model.ts
@@ -1,22 +1,27 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { AllowNull, Column, DataType, Model, PrimaryKey, Table } from 'sequelize-typescript';
 
 @Table
 export class User extends Model {
 	@Column
+	@PrimaryKey
 	pk_id: string;
 
-	@Column
-	created_date: string;
+	@Column(DataType.DATE)
+	created_date: Date;
 
 	@Column
+	@AllowNull(false)
 	emp_id: string;
 
 	@Column
+	@AllowNull(false)
 	first_name: string;
 
 	@Column
+	@AllowNull(false)
 	last_name: string;
 
 	@Column
+	@AllowNull(true)
 	middle_initial: string;
 }
--- src/Users/users.module.ts
+++ src/Users/users.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
+//import { User } from "./user.entity";
 import { User } from "./user.model";
 //import { UsersController } from "./users.controller";
-import { UsersService } from "./users.service";
+//import { UsersService } from "./users.service";
 
 @Module({
 	imports: [SequelizeModule.forFeature([User])],
 	exports: [SequelizeModule],
-	providers: [UsersService],
+	//providers: [UsersService],
 	//controllers: [UsersController],
 })
 export class UsersModule {}
